33ae25453406d61552cbbc3b2156bae2578e3c39
DanielZheng916 <113737591+DanielZheng916@users.noreply.github.com>
2023-06-27 16:01:27 -0400

    Init header

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,21 @@
+import Link from "next/link";
 
 
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <>
+        <header className="flex justify-between mb-4 items-center">
+            <h1 className="text-2xl">To Do</h1>
+            <Link 
+              href="/new"
+              className="border border-slate-100 rounded-md px-2 py-1
+              hover:bg-slate-500 focus-within:bg-slate-500 outline-none">
+                New
+            </Link>
+        </header>
+        <ul>
+
+        </ul>
+    </>
   )
 }
